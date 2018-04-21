8ee1ad1a9a065a27367f5523eb2f7b6cb983ac4e
Karthikeyan Govindaraj <karthikeyan.govindaraj@blackrock.com>
2018-04-21 01:50:13 -0700

    PDF Reader and scrapper :construction: still :unlock:

---
+++ index.js
@@ -0,0 +1,14 @@
+var PDFExtract = require('pdf.js-extract').PDFExtract;
+var fs = require('fs');
+var pdfExtract = new PDFExtract();
+pdfExtract.extract('./example.pdf', {} /* options*/, function (err, data) {
+    if (err) return console.log(err);
+    fs.writeFileSync('./example-output.json', JSON.stringify(data, null, '\t'));
+    var lines = PDFExtract.utils.pageToLines(data.pages[0], 2);
+    var rows = PDFExtract.utils.extractTextRows(lines);
+    var text = rows.map(function (row) {
+        return row.join('');
+    }).join('\n');
+    fs.writeFileSync('./example-output.txt', text);
+    console.log(JSON.stringify(data, null, '\t'));
+});
